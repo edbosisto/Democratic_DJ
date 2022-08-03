23a0862b970e71a324acc1341cbf172bce58f7c8
edbosisto <e.bosisto@gmail.com>
2022-08-03 13:08:25 +0200

    serializer complete to view rooms at api/home

--- democraticdj/api/views.py
+++ democraticdj/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import generics
-
 from .serializers import RoomSerializer
 from .models import Room
 
@@ -10,6 +9,6 @@ def index(request):
     pass
 
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
