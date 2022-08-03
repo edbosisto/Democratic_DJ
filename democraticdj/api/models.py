91fa52219cf5636c38952de8e43e7914664b6787
edbosisto <e.bosisto@gmail.com>
2022-08-03 12:11:51 +0200

    Room model created

--- democraticdj/api/models.py
+++ democraticdj/api/models.py
@@ -1,3 +1,12 @@
+from email.policy import default
 from django.db import models
+from django.forms import BooleanField
 
 # Create your models here.
+
+class Room(models.Model):
+    code = models.CharField(max_length=10, default="", unique=True)
+    host = models.CharField(max_length=40, unique=True)
+    guest_pause = BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_time = models.DateTimeField(auto_now_add=True)
