a586a4feb1758f9f28821a49436978bbb484b2b6
edbosisto <e.bosisto@gmail.com>
2022-08-10 11:07:27 +0200

    joinroompage.js setup

--- democraticdj/frontend/src/components/JoinRoomPage.js
+++ democraticdj/frontend/src/components/JoinRoomPage.js
@@ -1,11 +1,59 @@
 import React, { Component } from "react";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
+
+// A page to join a room
 export default class JoinRoomPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: "",
+            error: "",
+        };
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+        this.roomButtomPressed = this.roomButtomPressed.bind(this);
     }
 
     render() {
-        return <p>This is the Join Room Page</p>
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join A Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField 
+                        error={this.state.error}
+                        label="Code"
+                        placeholder="Enter A Room"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                        onChange={this.handleTextFieldChange}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary" onClick={this.roomButtomPressed}>
+                        Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
+                </Grid>
+            </Grid>
+        );
+    }
+    // The logic for this component. 
+    // This method captures what the current room code is.
+    handleTextFieldChange(e) {
+        this.setState({
+            roomCode: e.target.value
+        })
+    }
+    // Handles the event when enter room button is pressed. Sends to backend, asks does the room exist.
+    roomButtomPressed() {
+        console.log(this.state.roomCode)
     }
 }
